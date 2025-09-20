3bcaf13f379b895abef5aab71e00d92674c3ee9f
rameliaaa <s4133445@student.rmit.edu.au>
2025-09-20 20:51:04 +1000

    edited style.css

--- media player/main.js
+++ media player/main.js
@@ -8,21 +8,19 @@ const totalTimeText = document.querySelector("#totalTimeFeedback");
 /*when JS loads remove defult controls */
 VideoElement.removeAttribute("controls");
 
-//I want to update the totqal time based on tje currently loaded ,edia file
-//this will run when page loads, but if i wanted to change the file afterwards, i'd have to 
+//I want to update the totqal time based on the currently loaded ,edia file
+//this will run when page loads, but if i wanted to change the file afterwards, i'd have to
 // update there too
 VideoElement.addEventListener("canplay", updateTotalTime);
 
-
-function updateTotalTime(){
-    let videoSeconds = VideoElement.duration;
-    let totalMin = Math.floor (videoSeconds / 60);
-    let totalSec = videoSeconds % 60;
-    if(totalSec < 10){
-        totalSec = "0" = totalSec;
-    }
-   currentTimeText.textContent = `${totalMin} :${totalSec}`;
-
+function updateTotalTime() {
+  let videoSeconds = VideoElement.duration;
+  let totalMin = Math.floor(videoSeconds / 60);
+  let totalSec = videoSeconds % 60;
+  if (totalSec < 10) {
+    totalSec = "0" + totalSec;
+  }
+  totalTimeText.textContent = `${totalMin} :${totalSec}`;
 }
 /*
 Play/pause button behaviour:\When I click it begins the playback of the mmedia file
@@ -52,9 +50,35 @@ i should be able to click and jump to particular time
 
 function updateTimeline() {
   console.log(VideoElement.currentTime);
-  /* percentage of total time */
-  let timePercent = VideoElement.currentTime / VideoElement.duration;
-  //timeline.value= 0;
+  /* find percentage of total time */
+  let timePercent = (VideoElement.currentTime / VideoElement.duration) * 100;
+  //console.log(timePercent);
+  timeline.value = timePercent;
+  updateCurrentTime();
+}
+
+function updateCurrentTime() {
+  let videoSeconds = VideoElement.currentTime;
+  let totalMin = Math.floor(videoSeconds / 60);
+  let totalSec = Math.floor(videoSeconds % 60);
+  if (totalSec < 10) {
+    totalSec = "0" + totalSec;
+  }
+  currentTimeText.textContent = `${totalMin}:${totalSec}`;
 }
 
 VideoElement.addEventListener("timeupdate", updateTimeline);
+
+// find when I click my timeline and then jump to appropriate time
+timeline.addEventListener("click", jumpToTime);
+
+function jumpToTime(ev) {
+  // find how far from the left we clicked
+  let clickX = ev.offsetX;
+  // find how wide my timeline is
+  let timeLineWidth = timeline.offsetWidth;
+  // find the ratio of click to width
+  let clickPercent = clickX / timeLineWidth;
+  // update the current time
+  videoElement.currentTime = videoElement.duration * clickPercent;
+}
